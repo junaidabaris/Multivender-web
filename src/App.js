ef6d1ca54d3cb55974011e13c9f3a81a02dac2c6
junaid akhtar1 <bewithjuniad@gmail.com>
2023-01-18 18:12:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import PageNotFoundPage from './pages/page-not-found';
 import ProductDetailPage from './pages/product-detail';
 import Footer from './common/footer/Footer';
 import ProductsPage from './pages/products';
+import MainVender from './venderFile/vender-page/mainPage';
+import { useState } from 'react';
 function App() {
+  const [show, setshow] = useState(true)
   return (
     <>
-      <Header />
+      {show && <Header />}
       <Routes>
         <Route path='/' element={<Navigate to="/home" />} />
         <Route path='/home' element={<HomePage />} />
         <Route path='*' element={<Navigate to="/page-not-found" />} />
         <Route path='page-not-found' element={<PageNotFoundPage />} />
         <Route path='/products' element={<ProductsPage />} />
-        <Route path='/product-detail' element={<ProductDetailPage />} /> 
+        <Route path='/product-detail' element={<ProductDetailPage />} />
+        <Route path='/venderLogin' element={<MainVender setshow={setshow}/>} />
       </Routes>
-      <Footer />
+      {show && <Footer />}
     </>
   );
 }
---
+++ src/venderFile/vender-page/mainPage/index.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+function MainVender({setshow}) {
+    useEffect(() => {
+        setshow(false)
+    }, [])
+    return <>
+        hello vender
+    </>
+}
+export default MainVender
